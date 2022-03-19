c52a3f99ffbc326080a1223a3cf6f5e7a623291d
LeskovarLukas <leskovar.lukas@student.htlwrn.ac.at>
2022-03-20 00:33:59 +0100

    adding CLI11

--- src/tls_server/main.cpp
+++ src/tls_server/main.cpp
@@ -4,6 +4,7 @@
 #include <spdlog/spdlog.h>
 
 #include "BigInt/BigInt.hpp"
+#include "CLI11.hpp"
 
 #include "pipe.h"
 #include "utility.h"
@@ -12,14 +13,34 @@
 
 void handle_socket(asio::ip::tcp::socket&);
 
-int main() {
-    spdlog::set_level(spdlog::level::debug);
+int main(int argc, char* argv[]) {
+    CLI::App app{"tls_server"};
+
+    int port = 4433;
+    spdlog::level::level_enum log_level = spdlog::level::info;
+    std::map<std::string, spdlog::level::level_enum> log_level_map = {
+        {"trace", spdlog::level::trace},
+        {"debug", spdlog::level::debug},
+        {"info", spdlog::level::info},
+        {"warn", spdlog::level::warn},
+        {"error", spdlog::level::err},
+        {"critical", spdlog::level::critical}
+    };
+
+    app.add_option("-p,--port", port, "Port");
+    app.add_option("-l,--log-level", log_level, "Log level")->transform(CLI::CheckedTransformer(log_level_map, CLI::ignore_case));
+
+
+    CLI11_PARSE(app, argc, argv);
+
+
+    spdlog::set_level(log_level);
     std::vector<std::thread> threads;
     std::vector<asio::ip::tcp::socket*> sockets;
 
     try {
         asio::io_context io_context;
-        asio::ip::tcp::endpoint endpoint{asio::ip::tcp::v4(), 4433};
+        asio::ip::tcp::endpoint endpoint{asio::ip::tcp::v4(), (asio::ip::port_type)port};
         asio::ip::tcp::acceptor acceptor{io_context, endpoint};
     
         try {
