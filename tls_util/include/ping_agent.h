a30aa4b611efb566abac0ec4ff6ac0af4de002db
Marcel Dinhof <44982865+Marschell01@users.noreply.github.com>
2022-04-10 21:33:38 +0200

    Update ping_agent.h

--- tls_util/include/ping_agent.h
+++ tls_util/include/ping_agent.h
@@ -1,3 +1,13 @@
+/*
+author: Leskovar Lukas
+matnr: i17057
+file: ping_agent.h
+desc: THis module implements the ping agent to detect timeout.
+date: 2022-04-06
+class: 5b
+catnr: 10
+*/
+
 #include "session.h"
 #include "tls_observer.h"
 
